7e21ec012e583c6e9d6249f6586fe728cf4d5ba1
silv4b <brunno.linkin@gmail.com>
2024-12-05 09:28:08 -0300

    correção de migrations

--- database/migrations/2024_12_05_122314_create_schedulings_table.php
+++ database/migrations/2024_12_05_122314_create_schedulings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,7 @@ return new class extends Migration
         Schema::create('schedulings', function (Blueprint $table) {
             $table->id();
 
-            $table->string('description');  
+            $table->string('description');
             $table->dateTime('scheduled_at')->unique()->nullable();
             $table->enum('status', ['Pending', 'In Progress', 'Cancelled', 'Completed'])->default('Pending');
             $table->string('estimated_duration', 30)->nullable();
